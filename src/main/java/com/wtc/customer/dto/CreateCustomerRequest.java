3c3dd1c518926ed9fdc22b68028fd467adae1be9
Luiz Otavio <luizsoc123@gmail.com>
2026-03-20 18:57:26 -0300

    feat(customer): create CreateCustomerRequest DTO

--- src/main/java/com/wtc/customer/dto/CreateCustomerRequest.java
+++ src/main/java/com/wtc/customer/dto/CreateCustomerRequest.java
@@ -1,5 +1,16 @@
 package com.wtc.customer.dto;
+import jakarta.validation.constraints.NotBlank;
 
 public class CreateCustomerRequest {
-    
+    @NotBlank(message = "Nome é obrigatório")
+    private String name;
+
+    @NotBlank(message = "Documento é obrigatório")
+    private String document;
+
+    private Boolean vip;
+    private Boolean fidelidade;
+    private Boolean ativo;
+
+    // getters e setters
 }
