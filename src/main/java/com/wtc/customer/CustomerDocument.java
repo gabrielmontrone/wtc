e7b24f5189ea0457e2ec63c2c334f187ad416ee8
Luiz Otavio <luizsoc123@gmail.com>
2026-03-20 19:03:23 -0300

    feat(customer): create CustomerDocument model

--- src/main/java/com/wtc/customer/CustomerDocument.java
+++ src/main/java/com/wtc/customer/CustomerDocument.java
@@ -1,5 +1,23 @@
 package com.wtc.customer;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
 
+@Document(collection = "customers")
 public class CustomerDocument {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String document;
+
+    private Boolean vip;
+    private Boolean fidelidade;
+    private Boolean ativo;
+
+    private LocalDateTime createdAt;
+
+    // getters e setters
     
 }
