ea6fe3888cb2eecf28ce80d02b96df3ee1a95cc6
Luiz Otavio <luizsoc123@gmail.com>
2026-03-20 19:00:36 -0300

    feat(customer): create CustomerResponse DTO

--- src/main/java/com/wtc/customer/dto/CustomerResponse.java
+++ src/main/java/com/wtc/customer/dto/CustomerResponse.java
@@ -1,5 +1,18 @@
 package com.wtc.customer.dto;
+import java.time.LocalDateTime;
 
 public class CustomerResponse {
+
+    private String id;
+    private String name;
+    private String document;
+
+    private Boolean vip;
+    private Boolean fidelidade;
+    private Boolean ativo;
+
+    private LocalDateTime createdAt;
+
+    // getters e setters
     
 }
