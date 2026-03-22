ff25263d39aa1c59cfa6334769c022bf3626a584
Renison Ramos <renisonramosnegociosonline@gmail.com>
2026-03-21 21:37:02 -0300

    SCRUM-14: implementar api de historico de mensagens por cliente

--- src/main/java/com/wtc/message/MessageController.java
+++ src/main/java/com/wtc/message/MessageController.java
@@ -7,19 +7,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/messages")
 public class MessageController {
 
+
     private final SendMessageService sendMessageService;
+
+    private final ListMessageService listMessageService;
 
     public MessageController(SendMessageService sendMessageService) {
         this.sendMessageService = sendMessageService;
+        this.listMessageService = new ListMessageService();
     }
 
     @PostMapping
     public ResponseEntity<MessageResponse> send(@Valid @RequestBody SendMessageRequest request) {
         MessageResponse response = sendMessageService.execute(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    // --- MTD Historico---
+    @GetMapping("/customer/{customerId}")
+    public ResponseEntity<List<MessageResponse>> getHistory(@PathVariable String customerId) {
+        List<MessageResponse> history = listMessageService.execute(customerId);
+        return ResponseEntity.ok(history);
     }
 }
--- src/main/java/com/wtc/message/MessageRepository.java
+++ src/main/java/com/wtc/message/MessageRepository.java
@@ -2,5 +2,11 @@ package com.wtc.message;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends MongoRepository<MessageDocument, String> {
+
+    // Esta linha permite que você busque o histórico de mensagens de um cliente
+    List<MessageDocument> findByCustomerId(String customerId);
+
 }
